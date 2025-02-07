0a67818428c60c39009fe1165c2a0acd89e2adf2
ssomeshetty <bremblue189@gmail.com>
2025-02-08 01:57:24 +0530

    updated

--- find_a_house/find_a_house/urls.py
+++ find_a_house/find_a_house/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for find_a_house project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from listings.views import listing_list, listing_retreve, listing_create, listing_update
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listing_list, name='listing_list'),
+    path('listings/<int:pk>/', listing_retreve, name='listing_retreve'),
+    path('add-listing/', listing_create, name='listing_create'),
+    path('listings/<int:pk>/edit/', listing_update, name='listing_update'),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
