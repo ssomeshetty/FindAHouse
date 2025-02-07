4c58db72c105da539adb6520904937e0eb9bd864
ssomeshetty <bremblue189@gmail.com>
2025-02-08 01:58:29 +0530

    updated

--- find_a_house/listings/models.py
+++ find_a_house/listings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/listings/', blank=True, null=True)  # Add ImageField
+
+    def __str__(self):
+        return self.title
--- find_a_house/listings/views.py
+++ find_a_house/listings/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Listing
+from .forms import ListingForm
 
-# Create your views here.
+# View for listing all real estate listings
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request, 'listing_list.html', {'listings': listings})
+
+# View for showing a single listing's details
+def listing_retreve(request, pk):
+    listing = get_object_or_404(Listing, pk=pk)
+    return render(request, 'listing_detail.html', {'listing': listing})
+
+# View for creating a new listing
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listing_list')
+    else:
+        form = ListingForm()
+    return render(request, 'listing_add.html', {'form': form})
+
+# View for editing an existing listing
+def listing_update(request, pk):
+    listing = get_object_or_404(Listing, pk=pk)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listing_retreve', pk=listing.pk)
+    else:
+        form = ListingForm(instance=listing)
+    return render(request, 'listing_edit.html', {'form': form, 'listing': listing})
